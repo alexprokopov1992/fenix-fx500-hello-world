ef609ec478c2bcef5ad116949566dd8b3c9e7449
Денис <baden.i.ua@gmail.com>
2025-10-08 19:49:44 +0300

    Прибрано зайве, та досягнено роботу світлодіода на FXA500-03.

--- app/boards/fenix/fxa500_02/board.cmake
+++ app/boards/fenix/fxa500_02/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(nulink "-f")
+# board_runner_args(nulink "-f")
 # board_runner_args(pyocd "--target=m2354sjfae" "--frequency=4000000")
 board_runner_args(pyocd "--target=m2354sjfae" "--frequency=8000000")
 board_runner_args(jlink "--device=M2354SJFAE" "--speed=4000" "--reset-after-load")
--- app/boards/fenix/fxa500_03/board.cmake
+++ app/boards/fenix/fxa500_03/board.cmake
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(pyocd "--target=stm32h563vgtx" "--frequency=4000000")
+board_runner_args(jlink "--device=STM32H563VG" "--speed=4000" "--reset-after-load")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-board_runner_args(nulink "-f")
-board_runner_args(pyocd "--target=stm32h563vitx" "--frequency=4000000")
-board_runner_args(jlink "--device=STM32H563VI" "--speed=4000" "--reset-after-load")
+# board_runner_args(nulink "-f")
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
